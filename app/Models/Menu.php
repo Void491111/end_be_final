1aaf1578751f18928cc559914a5271630c49f7b5
hica lypse <voidw87@gmail.com>
2026-06-07 14:33:58 +0700

    membuat Menu models

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -7,6 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Menu extends Model
 {
-    /** @use HasFactory<\Database\Factories\MenuFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'description',
+        'price',
+        'image',
+        'is_available',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'price' => 'decimal:2',
+            'is_available' => 'boolean',
+        ];
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
