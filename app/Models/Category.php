ed66f246dde98b33bfab0b64b64ce94aaaeafaa5
hica lypse <voidw87@gmail.com>
2026-06-07 14:33:07 +0700

    membuat Category Models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,9 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
-    /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'icon',
+        'sort_order',
+        'is_active',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'sort_order' => 'integer',
+            'is_active' => 'boolean',
+        ];
+    }
+
+    // Auto-generate slug dari name kalo kosong
+    protected static function booted(): void
+    {
+        static::saving(function ($category) {
+            if (empty($category->slug)) {
+                $category->slug = Str::slug($category->name);
+            }
+        });
+    }
+
+    public function menus()
+    {
+        return $this->hasMany(Menu::class);
+    }
 }
