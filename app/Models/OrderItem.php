dc0131347859d2d54003a44aa8011b675e7a39af
hica lypse <voidw87@gmail.com>
2026-06-07 14:35:11 +0700

    membuat OrderItem models

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -2,9 +2,38 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'menu_id',
+        'menu_name_snapshot',
+        'price_snapshot',
+        'quantity',
+        'subtotal',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'price_snapshot' => 'decimal:2',
+            'subtotal' => 'decimal:2',
+            'quantity' => 'integer',
+        ];
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function menu()
+    {
+        return $this->belongsTo(Menu::class);
+    }
 }
