d3d23f5644500a58fbfbb01e953d91095083d5bb
unknown <voidw87@gmail.com>
2026-07-18 05:21:44 +0700

    update sisi pelanggan

--- config/cors.php
+++ config/cors.php
@@ -6,8 +6,12 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'http://10.244.132.254:3000',
     ],
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^http://10\.244\.\d+\.\d+:3000$#',
+        '#^http://192\.168\.\d+\.\d+:3000$#',
+    ],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
